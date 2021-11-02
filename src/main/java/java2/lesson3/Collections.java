1286f269caeb4b823a68db4404c41d3f12687fd4
evvol <Keshavoloshin@mail.ru>
2021-11-02 11:45:51 +0300

    Made homework 3 java2 fix

--- src/main/java/java2/lesson3/Collections.java
+++ src/main/java/java2/lesson3/Collections.java
@@ -2,31 +2,39 @@ package java2.lesson3;
 
 import java.util.*;
 
+
 public class Collections {
-    private static final String[] stringArray={"NOP","NOP1","NOP2","NOP3","NOP","NOP1","NOP2","NOP3","NOP4","NOP5"};
+    private static final String[] stringArray = {"NOP", "NOP1", "NOP2", "NOP3", "NOP", "NOP1", "NOP2", "NOP3", "NOP4", "NOP5" };
+
     public static void main(String[] args) {
 
         convertArray();
-        Phonebook phonebook=new Phonebook();
-        phonebook.add("Voloshin",123);
-        phonebook.add("Torshin",123);
-        phonebook.add("Voloshin",1234);
-        phonebook.add("Voloshin",1235);
-        phonebook.add("Vaskin",123);
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Voloshin", 123);
+        phonebook.add("Torshin", 123);
+        phonebook.add("Voloshin", 1234);
+        phonebook.add("Voloshin", 1235);
+        phonebook.add("Vaskin", 123);
         phonebook.get("Voloshin");
         phonebook.get("Torshin");
-        System.out.println(phonebook.getData());
+        phonebook.get("Tor");
+        phonebook.printAllPhoneBook();
 
 
     }
 
     private static void convertArray() {
-        Map<String,Integer> sortArray=new TreeMap<>();
-        for (String str:stringArray){
-            if(sortArray.containsKey(str)) sortArray.put(str,sortArray.get(str)+1);
-            else sortArray.put(str,1);
+        Map<String, Integer> sortArray = new TreeMap<>();
+        for (String str : stringArray) {
+//            if(sortArray.containsKey(str)) sortArray.put(str,sortArray.get(str)+1);
+//            else sortArray.put(str,1);
+            sortArray.merge(str, 1, Integer::sum);
         }
+        sortArray.forEach((str, number) -> {
+            System.out.println(str + ": " + number);
+        });
         System.out.println(sortArray);
+
     }
 
 }
--- src/main/java/java2/lesson3/Phonebook.java
+++ src/main/java/java2/lesson3/Phonebook.java
@@ -1,29 +1,44 @@
 package java2.lesson3;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Phonebook {
     private final Map<String, Set<Integer>> data;
 
     public Phonebook() {
-        data=new HashMap<>();
+        data = new TreeMap<>();
     }
 
-    public void add(String surname, Integer phoneNumber){
-        if (!data.containsKey(surname)) {
-            data.put(surname, new TreeSet<>());
-        }
-        data.get(surname).add(phoneNumber);
+    public void add(String surname, Integer phoneNumber) {
+        //ПЕРВЫЙ ВАРИАНТ
+//        data.merge(surname,new TreeSet<>(){{add(phoneNumber);}}, (oldValue, newValue) -> {
+//            oldValue.add(phoneNumber);
+//            return oldValue;
+//        });
+        // Втрой вариант
+//        if (!data.containsKey(surname)) {
+//            data.put(surname, new TreeSet<>());
+//        }
+//        data.get(surname).add(phoneNumber);
+        // Третий вариант
+        Set<Integer> numbers = getPhones(surname);
+        numbers.add(phoneNumber);
     }
 
-    public void get(String surname){
-        if (data.containsKey(surname)){
-            System.out.println(surname+" phones: "+data.get(surname));
-        }
-        else System.out.println("Surname "+surname+" not found");
+    private Set<Integer> getPhones(String surname) {
+        return data.computeIfAbsent(surname, key -> new TreeSet<>());
     }
 
-    public Map<String, Set<Integer>> getData() {
-        return data;
+    public void get(String surname) {
+        if (data.containsKey(surname)) {
+            System.out.println(surname + " phones: " + data.get(surname));
+        } else System.out.println("Surname " + surname + " not found");
+    }
+
+    public void printAllPhoneBook() {
+        data.forEach((surname, phones) -> System.out.println(surname + ": " + phones));
     }
 }
